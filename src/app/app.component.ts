728ca1fe6c6db8c2f3de5fd228bfcd3c64b9aace
Asmar Ismayilova <asmar.ismayilova2@technipenergies.com>
2023-12-02 19:17:42 +0400

    new project and plotly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
 
+PlotlyModule.plotlyjs = PlotlyJS;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'plotly-js';
+  
+  public graph = {
+    data: [
+        { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: {color: 'red'} },
+        { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+    ],
+    layout: {width: 320, height: 240, title: 'A Fancy Plot'}
+};
 }
